5732e8d83cd85597f3789d68a41e3d66d94a9c7a
star2dawn <sin001215@naver.com>
2022-12-12 17:08:38 +0900

    레스토랑 수정

--- js/index.js
+++ js/index.js
@@ -60,9 +60,10 @@
       let cnt = 0;
       let setId = 0;
 
-      const  $section10        = $('#section10');
+      const  $section10       = $('#section10');
       const  $slideContainer  = $('#section10 .slide-container');
       const  $slideWrap       = $('#section10 .slide-wrap');
+      const  n                = $('#section10 .slide').length-2;
       const  $prevBtn         = $('#section10 .prev-btn');
       const  $nextBtn         = $('#section10 .next-btn');
 
@@ -71,10 +72,10 @@
 
       //1. 메인슬라이드 함수
       function mainSlide(){
-         $slideWrap.stop().animate({ left: -100*cnt + '%' }, 600, function(){
-            if(cnt>=4) {cnt=1;}
-            if(cnt<=0) {cnt=3;}
-            $slideWrap.stop().animate({ left: -100*cnt + '%' }, 0); //fowrords + backwrords = both
+         $slideWrap.stop().animate({ left:`${ -100*cnt}%` }, 600, 'easeInOutExpo', function(){
+            if(cnt>=n) {cnt=0;}
+            if(cnt<=-1) {cnt=n-1;}
+            $slideWrap.stop().animate({ left:`${ -100*cnt}%` }, 0); //fowrords + backwrords = both
          })
 
       }
@@ -92,6 +93,7 @@
 
       //3. 자동타이머 함수
       function autoTimer(){
+         clearInterval(setId);
          setId = setInterval(nextCount, 3000); 
       }
       autoTimer();
@@ -122,7 +124,7 @@
       }
    });
 
-                 // 터치스와이프 & 드래그앤 드롭
+            // 터치스와이프 & 드래그앤 드롭
             // 5. 터치스와이프
             let touchS = null;
             let touchE = null;
@@ -132,26 +134,31 @@
             let dragS = null;
             let dragE = null;
             let winW = $(window).width();
+            let conW = $slideContainer.width(); 
 
+
+            // 데스크탑
             // 드래그앤드롭 터치스와이프는 컨테이너로 하고
             $slideContainer.on({
                mousedown(e){  // 터치시작
                   clearInterval(setId);
-                  winW = $(window).width(); // 터치할 때마다 창너비를 가져와서 변수에 저장
+                  winW = $(window).width();
+                  conW = $slideContainer.width(); // 터치할 때마다 창너비를 가져와서 변수에 저장
                   touchS = e.clientX; // 터치 시작
 
                   mouseD = true;       // 드래그 시작(마우스 다운 인경우만 실행)
-                  dragS = e.clientX - $slideWrap.offset().left-winW;  //  좌표값 - 슬라이드.offset().left-1903
+                  // dragS = e.clientX - $slideWrap.offset().left-conW + ((창너비-슬라이드너비)/2);  //  좌표값 - 슬라이드.offset().left-1903
+                  dragS = e.clientX - $slideWrap.offset().left - conW + ((winW-conW)/2);  //  좌표값 - 슬라이드.offset().left-1903
                },         
                mouseup(e){   // 터치끝
                   touchE = e.clientX;
 
-                  if( touchS - touchE > 30 ){ //다음 슬라이드
+                  if( touchS - touchE > 0 ){ //다음 슬라이드
                      if( !$slideWrap.is(':animated') ){
                         nextCount();
                      }               
                   }
-                  if( touchS - touchE < 30 ){ //이전 슬라이드
+                  if( touchS - touchE < 0 ){ //이전 슬라이드
                      if( !$slideWrap.is(':animated') ){
                         prevCount();
                      }               
@@ -166,18 +173,24 @@
                   // 메인슬라이드를 마우스로 잡고 끌고 놓기(드래그앤드롭)
                   dragE = e.clientX;
                   $slideWrap.css({ left : dragE-dragS });
+                  // 200픽셀 이하의 드래그는 동작안함
+                  // -      200
+                  // 부호 절대값 Math.abs()
+                  if(Math.abs(touchS - touchE) < 100){
+                     mainSlide();
+                  }
                },
                mouseleave(e){ // 마우스리브를 마우스 아웃으로 간주한다.
                   if(!mouseD) return;
 
                   touchE = e.clientX;
-                  if( touchS - touchE > 30 ){ //다음 슬라이드
+                  if( touchS - touchE > 0 ){ //다음 슬라이드
                      if( !$slideWrap.is(':animated') ){
                         nextCount();
                      }               
                   }
 
-                  if( touchS - touchE < 30 ){ //이전 슬라이드
+                  if( touchS - touchE < 0 ){ //이전 슬라이드
                      if( !$slideWrap.is(':animated') ){
                         prevCount();
                      }               
@@ -185,8 +198,53 @@
                   mouseD = false;    // 드래그 끝(마우스 업 인경우 끝)  
                }
             });
+
+            // 태블릿 & 모바일: 핑거링(손가락) 이벤트 => 터치이벤트
+            // 드래그앤드롭 터치스와이프는 컨테이너로 하고
+            $slideContainer.on({
+               touchstart(e){  // 터치시작
+                  clearInterval(setId);
+
+                  winW = $(window).width();
+                  conW = $slideContainer.width(); // 터치할 때마다 창너비를 가져와서 변수에 저장
+                  touchS = e.originalEvent.changedTouches[0].clientX; // 터치 시작
+
+                  mouseD = true;       // 드래그 시작(마우스 다운 인경우만 실행)
+                  dragS = e.clientX - $slideWrap.offset().left - conW + ((winW-conW)/2);  //  좌표값 - 슬라이드.offset().left-1903
+
+               },         
+               touchend(e){   // 터치끝
+                  touchE = e.originalEvent.changedTouches[0].clientX;
+                  if( touchS - touchE > 0 ){ //다음 슬라이드
+                     if( !$slideWrap.is(':animated') ){
+                        nextCount();
+                     }               
+                  }
+                  if( touchS - touchE < 0 ){ //이전 슬라이드
+                     if( !$slideWrap.is(':animated') ){
+                        prevCount();
+                     }               
+                  }
+                  mouseD = false;    // 드래그 끝(마우스 업 인경우 끝)  
+               },
+               touchmove(e){
+                  if(!mouseD) return;
+                  // 마우스 좌우 이동하는걸 마우스무브라한다.
+                  // 반드시 마우스 다운했을 때만 마우스무브 가 작동
+                  // 메인슬라이드를 마우스로 잡고 끌고 놓기(드래그앤드롭)
+                  dragE = e.originalEvent.changedTouches[0].clientX;
+                  $slideWrap.css({ left : dragE-dragS });
+                  // 200픽셀 이하의 드래그는 동작안함
+                  // -      200
+                  // 부호 절대값 Math.abs()
+                  if(Math.abs(touchS - touchE) < 100){
+                     mainSlide();
+                  }
+               }
+
+            });
      
-    },
+    }
 
   }
   Rest.init();
